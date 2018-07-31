fd5cf2cda165ce8a266ee27ee8b6565448bd3136
NegativeThings <cxmforprivateuse@gmail.com>
2018-07-31 08:33:48 +0800

    20180731 08:30

--- Commodity/Data/Data_Import_WindPy.py
+++ Commodity/Data/Data_Import_WindPy.py
@@ -11,10 +11,12 @@ sys.path.append(r'D:\CXM\Project_New\Commodity')
 sys.path.append(r'D:\CXM\Project_New\SQLLINK')
 import constant
 import SASQL
+import MSSQL
 
 
-dbdc = SASQL.DataCenter_Commodity()
-tbco = SASQL.Commodity
+# dbdc = SASQL.DataCenter_Commodity()
+# tbco = SASQL.Commodity
+dbdc = MSSQL.DB_DataCenter_Commodity()
 path = constant.path_Data_Sorted
 
 # 读取全部指标的WindId
@@ -23,7 +25,7 @@ nameli = xls.sheet_names
 print(nameli)
 xls_dic = {}
 for i in nameli:
-    xls_dic[i] = pd.read_excel(xls,sheet_name=i,header=0).fillna('#N/A')
+    xls_dic[i] = pd.read_excel(xls, sheet_name=i, header=0).fillna('#N/A')
 
 def cutlist(li, width=15):
     a = []
@@ -45,7 +47,7 @@ for i in nameli:
     li_index_cut = cutlist(li_index)
     for j in li_index_cut:
         str_index = ','.join(j) # 拼接全部ItemID，用于查询数据
-        a = w.edb(str_index, '19000101', '20180724')
+        a = w.edb(str_index, '19000101', '20180730')
         df = pd.DataFrame(columns=a.Codes, index=a.Times) #将wind返回的对象处理成dataframe
         for m in range(0,len(a.Codes)):
             df[a.Codes[m]] = a.Data[m]
@@ -53,32 +55,14 @@ for i in nameli:
             df0 = source[source['ItemID']==n]
             df1 = df[[n]].dropna()
             for x in range(0,df1.shape[0]):
-                record = tbco(Date=df1.index[x], CategoryID=df0.iloc[0][0], CategoryName=df0.iloc[0][1],ProductID=df0.iloc[0][2], ProductName=df0.iloc[0][3], ClassID=df0.iloc[0][4],ClassName=df0.iloc[0][5], ItemID=df0.iloc[0][6], ItemName=df0.iloc[0][7],Frequency=df0.iloc[0][8], Unit=df0.iloc[0][9], Source=df0.iloc[0][10],UpdateSource=df0.iloc[0][11],Data=df1.iloc[x][0])
-                dbdc.session.add(record)
-                dbdc.session.commit()
-dbdc.session.close()
+                try:
+                    query = "insert into Commodity(Date,CategoryID,CategoryName,ProductID, ProductName, ClassID,ClassName,ItemID,ItemName,Frequency,Unit,Source,UpdateSource,Data) values('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}',{})".format(df1.index[x], df0.iloc[0][0], df0.iloc[0][1],df0.iloc[0][2], df0.iloc[0][3], df0.iloc[0][4], df0.iloc[0][5], df0.iloc[0][6], df0.iloc[0][7],df0.iloc[0][8], df0.iloc[0][9], df0.iloc[0][10], df0.iloc[0][11],df1.iloc[x][0])
+                except IndexError as e:
+                    print("Wind代码为"+str(ItemID=df0.iloc[0][6])+"的字段没有数据")
+#                 record = tbco(Date=df1.index[x], CategoryID=df0.iloc[0][0], CategoryName=df0.iloc[0][1],ProductID=df0.iloc[0][2], ProductName=df0.iloc[0][3], ClassID=df0.iloc[0][4],ClassName=df0.iloc[0][5], ItemID=df0.iloc[0][6], ItemName=df0.iloc[0][7],Frequency=df0.iloc[0][8], Unit=df0.iloc[0][9], Source=df0.iloc[0][10],UpdateSource=df0.iloc[0][11],Data=df1.iloc[x][0])
+#                 dbdc.session.add(record)
+#                 dbdc.session.commit()
+# dbdc.session.close()
+                else:
+                    dbdc.ExecNonQuery(query)
 w.stop()
-# print(li_index)
-# print(str_index)
-
-
-
-    # a = w.edb('S5806088,S5806089', '20160101', '20180701')
-
-
-# a = w.edb('S5806088,S5806089','20160101','20180701')
-# df = pd.DataFrame(columns=a.Codes,index=a.Times)
-# for i in range(0, len(a.Codes)):
-#     df[a.Codes[i]] = a.Data[i]
-# print(df)
-
-# df0 = xls_dic['有色金属'][xls_dic['有色金属']['ItemID']=='S5806088']
-# df1 = df[['S5806088']]
-# df1 = df1.dropna()
-#
-# record = tbco(Date=df1.index[0],CategoryID=df0.iloc[0][0],CategoryName=df0.iloc[0][1],ProductID=df0.iloc[0][2],ProductName=df0.iloc[0][3],ClassID=df0.iloc[0][4],ClassName=df0.iloc[0][5],ItemID=df0.iloc[0][6],ItemName=df0.iloc[0][7],Frequency=df0.iloc[0][8],Unit=df0.iloc[0][9],Data=df1.iloc[0][0])
-#
-# dbdc.session.add(record)
-# dbdc.session.commit()
-#
-# w.stop()
