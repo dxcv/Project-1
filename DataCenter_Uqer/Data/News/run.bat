a9005d7017cb30abd09677500180f2421fa9c88d
CXM <redhansard@gmail.com>
2018-12-27 09:46:51 +0800

    20181227 0946

--- DataCenter_Uqer/Data/News/run.bat
+++
@@ -1,9 +0,0 @@
-echo start....>>D:\CXM\Project_New\DataCenter_Uqer\News\log.log
-echo %date% >>D:\CXM\Project_New\DataCenter_Uqer\News\log.log
-
-D:
-CD D:\CXM\Project_New\DataCenter_Uqer\News
-
-python GetNews.py
-
-echo finished....>>D:\CXM\Project_New\DataCenter_Uqer\News\log.log
--- DataCenter_Uqer/Research/VIX.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-计算隐式波动率
-"""
-
